8f3fe8ebc5a0ef93f458906f0df61af1dbf26208
Solomon Hilliard <shillia6@eagles.nccu.edu>
2015-03-20 00:11:26 -0400

    changes get to post

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   def index
-    @response = HTTParty.get('http://chefbuddy.herokuapp.com/api/v1/suggested_recipe/')
+    @response = HTTParty.post('http://chefbuddy.herokuapp.com/api/v1/suggested_recipe/')
   end
 
   def random
-    @response = HTTParty.get('http://chefbuddy.herokuapp.com/api/v1/suggested_recipe/')
+    @response = HTTParty.post('http://chefbuddy.herokuapp.com/api/v1/suggested_recipe/')
   end
 end
